a47a1bcc5d55749e3c348554bb44cc87ef44d528
Jonathan Harrison <jonathan.u.harrison@warwick.ac.uk>
2021-08-25 09:28:09 +0100

    main to run from command line args

--- R/main.R
+++ R/main.R
@@ -1,3 +1,4 @@
+args = commandArgs(trailingOnly=TRUE)
 library(rstan) 
 library(dplyr) 
 library(purrr)
@@ -21,32 +22,39 @@ source(here::here('R/extract_hidden_states.R'))
 ##############################
 #options etc
 dt=2.05
-identifier <- "JH_test_v111"
 run_analysis <- TRUE
 use_parallel <- FALSE
 run_changept_anyway <- FALSE
 num_iter <- 100
+identifier = args[1]
+path_to_folder = args[2]
+
+print(identifier)
+print(path_to_folder)
 #############################
 
-jobset_str <- here::here("data/OS_LLSM_200818_MC191_Untreated_2.04933s_per_frame//kittracking001-kitjobset_200825_DonaldDuck_auto_v125-OS_LLSM_200818_MC191_Untreated_capture10_flowdec_deconvolved.ome.csv")
-data_single_pair <- process_jobset(jobset_str,K=Inf,max_missing=0.25) %>%
-  filter(!is.na(SisterID)) #omit unpaired KTs
+#jobset_str <- here::here("data/OS_LLSM_200818_MC191_Untreated_2.04933s_per_frame//kittracking001-kitjobset_200825_DonaldDuck_auto_v125-OS_LLSM_200818_MC191_Untreated_capture10_flowdec_deconvolved.ome.csv")
+#data_single_pair <- process_jobset(jobset_str,K=Inf,max_missing=0.25) %>%
+#  filter(!is.na(SisterID)) #omit unpaired KTs
 
-g <- ggplot(data_single_pair, aes(x=Time, y=Position_1,color=factor(SisterID))) +
-  geom_line() +
-  facet_wrap(.~SisterPairID) +
-  theme_bw() +
-  theme(legend.position = "None",
-        strip.text.x = element_text(size = 8),
-        axis.text.x = element_text(angle=90)) +
-  labs(x="Time (s)",y="Position (um)")
-g
-ggsave(here::here("plots/200818_untreated_capture10_all_pairs.eps"),width=7,height=7)
+jobset_str_list <- list.files(path = path_to_folder,pattern="\\.csv$",full.names=TRUE)
+K_list <- rep(NA,length(jobset_str_list))
 
+#g <- ggplot(data_single_pair, aes(x=Time, y=Position_1,color=factor(SisterID))) +
+#  geom_line() +
+#  facet_wrap(.~SisterPairID) +
+#  theme_bw() +
+#  theme(legend.position = "None",
+#        strip.text.x = element_text(size = 8),
+#        axis.text.x = element_text(angle=90)) +
+#  labs(x="Time (s)",y="Position (um)")
+#g
+#ggsave(here::here("plots/200818_untreated_capture10_all_pairs.eps"),width=7,height=7)
 
-#fit changepoint model and hierarchical model
-out <- run_anaphase_models_for_ith_jobset(1,jobset_str,Inf,here::here("fits"),identifier,run_analysis,use_parallel,run_changept_anyway,dt,num_iter)
-
+for (i in seq_along(jobset_str_list)){
+  #fit changepoint model and hierarchical model
+  out <- run_anaphase_models_for_ith_jobset(i,jobset_str_list[i],K_list[i],here::here("fits"),identifier,run_analysis,use_parallel,run_changept_anyway,dt,num_iter)
+}  
 #extract hidden states
-sigma_sim <- extract_hidden_states(out)
+#sigma_sim <- extract_hidden_states(out)
 
