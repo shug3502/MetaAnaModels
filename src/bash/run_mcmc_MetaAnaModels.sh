942e14b3283cee1df5d801a52dc274d080e5004d
Jonathan Harrison <jonathanharrison@isis.warwick.ac.uk>
2021-09-01 11:20:35 +0100

    Add run scripts

---
+++ src/bash/run_mcmc_MetaAnaModels.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=MetaAnaModels_2s_hierarchical			  #see pauls email or eg here https://help.rc.ufl.edu/doc/Sample_SLURM_Scripts
+#SBATCH --time=180:00:00
+#SBATCH --mem-per-cpu=32g
+#SBATCH --export=ALL
+#SBATCH --ntasks=1                   			  # Run on a single CPU
+
+pwd; hostname; date
+module load R
+identifier="${USER}_MetaAnaModels_2s_v421"
+Rscript R/main.R "${identifier}" "${path_to_data}"
